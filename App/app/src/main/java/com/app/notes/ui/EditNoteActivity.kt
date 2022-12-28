256218c89fc1cfa1e1643d553ac80bc075e220c5
ericasaline <ericasaline@gmail.com>
2022-12-28 18:15:40 -0300

    Layout

---
+++ App/app/src/main/java/com/app/notes/ui/EditNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.app.notes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.notes.R
+
+class EditNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_note)
+    }
+}
